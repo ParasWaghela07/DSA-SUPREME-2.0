1e3ae2d70a1cf7e85bd3fe93b8f0497d8beb5a15
Paras Waghela <paraswaghela777@gmail.com>
2023-12-26 10:35:32 +0530

    bst assignment

--- BST (WEEK 14)/dead_end.cpp
+++ BST (WEEK 14)/dead_end.cpp
@@ -39,47 +39,34 @@ class Solution{
     }
 };
 
-//OWN BUT WRONG
-class Solution{
-  public:
-    map<int,bool>mp;
-    bool flag=false;
-    
-    void solve(Node* root){
-        if(root==NULL){
-            return;
-        }
-        mp[root->data]=true;
-       
-        if(root->left==NULL && root->right==NULL){
-            if(root->data==1){
-                flag=true;
-                return;
-            }
-            int element=root->data;
-            int prev=element-1;
-            int next=element+1;
-            
-            if(mp[prev]==true && mp[next]==true){
-                flag=true;
-                return;
-            }
-        }
-        
-        solve(root->left);
-        solve(root->right);
-        
+//METHOD 2
+class Solution {
+public:
+    bool isDeadEnd(Node* root) {
+        return isDeadEndHelper(root, 0, INT_MAX);
     }
-    bool isDeadEnd(Node *root)
-    {
-        if(root==NULL){
+    
+    bool isDeadEndHelper(Node* node, int minVal, int maxVal) {
+        if (node == nullptr) {
+            return false; // Base case: Node doesn't exist, no dead end found
+        }
+        
+        // Check if the current node leads to a dead end
+        if (minVal == maxVal) {
+            return true; // Dead end found
+        }
+        
+        // Traverse left subtree with updated min and max values
+        bool leftDeadEnd = isDeadEndHelper(node->left, minVal, node->data - 1);
+        
+        // Traverse right subtree with updated min and max values
+        bool rightDeadEnd = isDeadEndHelper(node->right, node->data + 1, maxVal);
+        
+        // If either subtree has a dead end, return true
+        if (leftDeadEnd || rightDeadEnd) {
             return true;
         }
-        if(root->data==1){
-            return false;
-        }
-        solve(root);
-        return flag;
         
+        return false; // No dead end found
     }
 };
