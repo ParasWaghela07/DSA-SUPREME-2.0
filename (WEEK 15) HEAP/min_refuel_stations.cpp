b46dff1c272d9fa17dd68e4a186c9842d0bf5cd0
Paras Waghela <paraswaghela777@gmail.com>
2024-01-07 20:00:15 +0530

    HEAP ASSIGNMENT

--- (WEEK 15) HEAP/min_refuel_stations.cpp
+++ (WEEK 15) HEAP/min_refuel_stations.cpp
@@ -91,3 +91,42 @@ public:
         }
     }
 };
+
+
+int minRefuelStops(int target,int startFuel,vector<vector<int>>&stations){
+    int stops=0;
+    int dist=0;
+    int i=0;
+    int fuel=startFuel;
+    priority_queue<pair<int,int>>pq;
+
+    while(true){
+        while(i<stations.size()){
+            if(stations[i][0] <= dist + fuel){
+                pq.push({station[i][1],stations[i][0]});
+            }
+            else{
+                break;
+            }
+
+            i++;
+        }
+
+        dist+=fuel;
+        fuel=0;
+
+        if(dist>=target) break;
+
+        if(pq.empty()){
+            stops=-1;
+            break;
+        }
+
+        auto&top=pq.top();
+        fuel=(dist-top.second) + top.first;
+        dist=top.second;
+        pq.pop();
+        ++stops;
+    }
+    return stops;
+}
