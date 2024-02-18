311edd2f795e3265c8ec22b077b3e22ed6b76708
Paras Waghela <paraswaghela777@gmail.com>
2024-02-18 19:29:08 +0530

    Backtracking

---
+++ (WEEK 8) DnC & BACKTRACKING/permutation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void prmt(string &s,int i){
+    if(i>=s.size()){
+        cout<<s<<endl;
+        return;
+    }
+
+    for(int j=i;j<s.size();j++){
+        swap(s[i],s[j]);
+        prmt(s,i+1);
+        swap(s[i],s[j]);
+    }
+}
+
+int main(){
+    string s="abc";
+    int index=0;
+    prmt(s,index);
+
+}
