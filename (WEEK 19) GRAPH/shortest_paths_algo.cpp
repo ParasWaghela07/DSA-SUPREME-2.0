55f71eebe36ba7e7458e8907b88ed415971fa6ce
Paras Waghela <paraswaghela777@gmail.com>
2024-03-03 02:00:48 +0530

    dijkstra

--- (WEEK 19) GRAPH/shortest_paths_algo.cpp
+++ (WEEK 19) GRAPH/shortest_paths_algo.cpp
@@ -91,32 +91,88 @@ class graph{
 			cout << i <<  " ";
 		}
     }
+
+    void dijkstraShortestDistance(int n, int src, int dest) {
+		vector<int> dist(n+1, INT_MAX);
+		set<pair<int,int> > st;
+
+		//INTIAL STATE -> (0,src)
+		st.insert({0,src});
+		dist[src] = 0;
+
+		//distance updation logic 
+		while(!st.empty()) {
+			auto topElement = st.begin();
+			pair<int, int> topPair = *topElement;
+			int topDist = topPair.first;
+			int topNode = topPair.second;
+			//remove the topNode
+			st.erase(st.begin());
+
+			//update distance of nbr
+			for(pair<int,int> nbrPair: adj[topNode]) {
+				int nbrNode = nbrPair.first;
+				int nbrDist = nbrPair.second;
+				if(topDist + nbrDist < dist[nbrNode]) {
+					//found a new shorter distnace
+					//now update: 
+					
+					// -> set ki entry
+					//find previousEntry and delete it
+					auto previousEntry = st.find({dist[nbrNode], nbrNode});
+					if(previousEntry != st.end()) {
+						// entry present in set
+						st.erase(previousEntry);
+					}
+					// -> dist ka array
+					dist[nbrNode] = topDist + nbrDist;
+					//create a new entry in the set for new distance
+					st.insert({dist[nbrNode], nbrNode});
+				}
+			}
+		}
+		cout << "Shortest Distance from " << src << " Node to " << dest << " Node: " << dist[dest] << endl;
+
+
+	}
 };
 
 int main(){
     graph g;
-    g.edge(0,1,5,1);
-    g.edge(0,2,2,1);
-    g.edge(2,1,3,1);
-    g.edge(1,3,3,1);
-    g.edge(2,4,11,1);
-    g.edge(4,3,14,1);
+    // g.edge(0,1,5,1);
+    // g.edge(0,2,2,1);
+    // g.edge(2,1,3,1);
+    // g.edge(1,3,3,1);
+    // g.edge(2,4,11,1);
+    // g.edge(4,3,14,1);
 
-    int n=5;
+    // int n=5;
 
     //g.print();
-    stack<int>st;
-    unordered_map<int,bool>visited;
+    // stack<int>st;
+    // unordered_map<int,bool>visited;
 
-    g.toposortDfs(0,st,visited);
+    // g.toposortDfs(0,st,visited);
 
     // while(!st.empty()){
     //     cout<<st.top()<<" ";
     //     st.pop();
     // }
 
-    g.shortestPathDfs(st,n);
+    // g.shortestPathDfs(st,n);
+    g.edge(1,6,14,0);
+	g.edge(1,3,9,0);
+	g.edge(1,2,7,0);
+	g.edge(2,3,10,0);
+	g.edge(2,4,15,0);
+	g.edge(3,4,11,0);
+	g.edge(6,3,2,0);
+	g.edge(6,5,9,0);
+	g.edge(5,4,6,0);
 
-    
+    int n = 6;
+	int src = 6;
+	int dest = 4;
+	g.dijkstraShortestDistance(n, src, dest);
 
 }
