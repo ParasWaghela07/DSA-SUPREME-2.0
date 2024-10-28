15baa77bf2082901b06da0af841b4035b146e54a
Paras Waghela <paraswaghela777@gmail.com>
2024-10-28 19:22:38 +0530

    graph class2 done cycleDet

--- (WEEK 19) GRAPH/detect_cycle_inUndG.cpp
+++ (WEEK 19) GRAPH/detect_cycle_inUndG.cpp
@@ -12,14 +12,18 @@ bool checkcyclebyBfs(int &src,unordered_map<int,bool>&vis,vector<int>adj[]){
             q.pop();
             
             for(auto it:adj[front]){
+                if(it==parent[front]) continue; // FLAG 1 if we uuse this then remove FLAG2 and just return true in else condition
                 if(!vis[it]){
                     q.push(it);
                     vis[it]=true;
                     parent[it]=front;
                 }
-                else if(it!=parent[front]){
+                else if(it!=parent[front]){ // FLAG 2 if we use this then remove FLAG1
                     return true;
                 }
+                // else if(parent[it]!=-1 && parent[it]!=front){ ---------why not this
+                //     return true;
+                // }
             }
         }
         
@@ -43,14 +47,16 @@ bool checkcyclebyBfs(int &src,unordered_map<int,bool>&vis,vector<int>adj[]){
     bool checkcyclebyDfs(int src,unordered_map<int,bool>&vis,vector<int>adj[],int parent){
         vis[src]=true;
         for(auto it:adj[src]){
+            if(it==parent) continue; // if we do not write this then uncomment last else if also remove last 'else' 
             if(!vis[it]){
                 // vis[it]=true;
                 bool ans = checkcyclebyDfs(it,vis,adj,src);
                 if(ans) return ans;
             }
-            else if(it!=parent){
-                return true;
-            }
+            else return true;
+            // else if(it!=parent){
+            //     return true;
+            // }
         }
         
         return false;
