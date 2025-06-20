d72335beaada063c3f31d3ede2e66be98cef6959
Paras Waghela <paraswaghela777@gmail.com>
2025-06-20 16:39:12 +0530

    tree rev done

--- (WEEK 13) TREE/N_ary_Tree.cpp
+++ (WEEK 13) TREE/N_ary_Tree.cpp
@@ -1,27 +1,61 @@
 #include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
     public:
-    int* arr;
+    vector<Node*>c;
     int size;
+    int val;
     
-
-    Node(int size){
+    Node(int size,int data){
         this->size=size;
-        arr=new int[size];
+        this->c.resize(size);
+        this->val=data;
     }
 };
 
 Node* InsertNode(){
+
+    int data;
+    cout<<"Enter the value of the node : ";
+    cin>>data;
+
+    int size;
+    cout<<"Enter the size of the node : ";
+    cin>>size;
     
+    Node* newNode=new Node(size,data);
+    
+    cout<<"Enter the elements of the node : ";
+    for(int i=0;i<size;i++){
+        newNode->c[i]=InsertNode();
+    }
+    return newNode;
 }
 
+void bfs(Node*root){
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        int size=q.size();
+        while(size--){
+            Node* current=q.front();
+            q.pop();
+            cout<<current->val<<" ";
+            
+            for(int i=0;i<current->size;i++){
+                q.push(current->c[i]);
+            }
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     cout<<"Enter the Root element : "<<endl;
     Node* root=InsertNode();
-    
-    
-
+    cout<<"Tree created successfully!"<<endl;
+    bfs(root);
 }
--- (WEEK 13) TREE/burning_tree.cpp
+++ (WEEK 13) TREE/burning_tree.cpp
@@ -1,5 +1,55 @@
 class Solution {
   public:
+    void solve(Node*root,unordered_map<int,vector<int>>&adj){
+        if(!root) return;
+        
+        if(root->left){
+            adj[root->data].push_back(root->left->data);
+            adj[root->left->data].push_back(root->data);
+        }
+        if(root->right){
+            adj[root->data].push_back(root->right->data);
+            adj[root->right->data].push_back(root->data);
+        }
+        
+        solve(root->left,adj);
+        solve(root->right,adj);
+    }
+    
+    int minTime(Node* root, int target) {
+        unordered_map<int,vector<int>>adj;
+        solve(root,adj);
+        
+        queue<int>q;
+        unordered_set<int>st;
+        
+        q.push(target);
+        int cnt=0;
+        
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int front=q.front();
+                q.pop();
+                
+                st.insert(front);
+                for(auto &it:adj[front]){
+                    if(!st.count(it)){
+                        q.push(it);
+                    }
+                }
+            }
+            
+            cnt++;
+        }
+        
+        return cnt-1;
+    }
+};
+
+
+class Solution {
+  public:
   
   
     Node* MakeNodeToParentMappingAndFindTarget(Node* root, unordered_map<Node*,Node*>&parentMap, int target){
